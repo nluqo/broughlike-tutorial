3f76b442ebd2aa5ba90af0aa72528d5b3bb69b9c
Jeremiah Reid <jeremiah.reid@gmail.com>
2019-06-16 10:14:22 -0400

    Stage 4 in progress

--- test/js/monster.js
+++ test/js/monster.js
@@ -5,7 +5,11 @@ class Monster{
        this.hp = hp;
 	}
 
-update(){
+    update(){
+           this.doStuff();
+       }
+   
+       doStuff(){
           let neighbors = this.tile.getAdjacentPassableNeighbors();
           
           neighbors = neighbors.filter(t => !t.monster || t.monster.isPlayer);
@@ -19,16 +23,45 @@ update(){
 
 	draw(){
        drawSprite(this.sprite, this.tile.x, this.tile.y);
+      this.drawHp();
 	}
+
+  drawHp(){
+       for(let i=0; i<this.hp; i++){
+           drawSprite(
+               9,
+               this.tile.x + (i%3)*(5/16),
+               this.tile.y - Math.floor(i/3)*(5/16)
+           );
+       }
+   }    
 
    tryMove(dx, dy){
        let newTile = this.tile.getNeighbor(dx,dy);
        if(newTile.passable){
            if(!newTile.monster){
                this.move(newTile);
+           }else{
+               if(this.isPlayer != newTile.monster.isPlayer){
+                  this.attackedThisTurn = true;
+                   newTile.monster.hit(1);
+               }
            }
            return true;
        }
+   }
+
+   hit(damage){
+       this.hp -= damage;
+       if(this.hp <= 0){
+           this.die();
+       }
+   }
+
+   die(){
+       this.dead = true;
+       this.tile.monster = null;
+       this.sprite = 1;
    }
 
    move(tile){
@@ -62,26 +95,35 @@ class Bird extends Monster{
      }
  }
  
- class Snake extends Monster{
-     constructor(tile){
-         super(tile, 5, 1);
-     }
- }
- 
- class Tank extends Monster{
-     constructor(tile){
-         super(tile, 6, 2);
-     }
- }
- 
- class Eater extends Monster{
-     constructor(tile){
-         super(tile, 7, 1);
-     }
- }
- 
- class Jester extends Monster{
-     constructor(tile){
-         super(tile, 8, 2);
-     }
- }
+class Snake extends Monster{
+   constructor(tile){
+       super(tile, 5, 1);
+   }
+
+   doStuff(){
+       this.attackedThisTurn = false;
+       super.doStuff();
+
+       if(!this.attackedThisTurn){
+           super.doStuff();
+       }
+   }
+}
+
+class Tank extends Monster{
+   constructor(tile){
+       super(tile, 6, 2);
+   }
+}
+
+class Eater extends Monster{
+   constructor(tile){
+       super(tile, 7, 1);
+   }
+}
+
+class Jester extends Monster{
+   constructor(tile){
+       super(tile, 8, 2);
+   }
+}
